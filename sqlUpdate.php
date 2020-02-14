a42d079e6b2ca356e2022405eab909c87fab29ba
sciencegum5 <32943597+sciencegum5@users.noreply.github.com>
2020-02-14 09:06:32 -1000

    why was this python, wtf me

--- sqlUpdate.php
+++ sqlUpdate.php
@@ -1,26 +1,23 @@
 <?php
-# Import pymysql library
-import pymysql
+    $servername = "localhost"; //use localhost because file is on the server
+    $username = "user";
+    $password = "password";
+    $dbname = "school";  //database name
 
-# Open database connection
-db = pymysql.connect(host="localhost", user="user", passwd="password", db="school")
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname); //instantiate mysqli object
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error); //turn off connection on our end
+    }
+    $sql = "UPDATE students SET age=15, gradeLevel=10 WHERE name='Bartholomew'";
+    //$sql = "INSERT INTO led (toggleOne)
+    //VALUES (1)";
+    if ($conn->query($sql) === TRUE) { //pass sql query into object
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-# Create a cursor object using cursor() method
-cursor = db.cursor()
-
-cur = db.cursor()
-
-# Select everything
-sql = "UPDATE students SET age=15, gradeLevel=10 WHERE name='Bartholomew'"
-
-# Execute sql statement
-cur.execute(sql)
-
-#rows = cur.fetchall() #request all data from rows
-#for row in rows: #output all data
-#	print(row)
-
-# Disconnect from server
-cur.close()
-db.close()
+    $conn->close();
 ?>
